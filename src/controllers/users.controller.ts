92fbee623baf4ad5dad3aee93e386022bde1d0fc
keside <wilsonezeala@yahoo.com>
2023-06-02 06:33:12 +0100

    :feat create new user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,17 +1,58 @@
 import { Request, Response } from 'express';
 import userModel from '../models/user.model';
 import type { IUserCreateBody } from '../types';
+import jwt from 'jsonwebtoken';
+
+import bcrypt from 'bcrypt';
+
+const { SALT_ROUNDS, PRIVATE_KEY } = process.env;
 
 export async function createUser(req: Request, res: Response) {
   const newUserDetails: IUserCreateBody = req.body;
 
+  //Email and Password must be provided
   if (!newUserDetails.email || !newUserDetails.password) {
     return res.status(400).json({ error: 'email and password required' });
   }
-  try {
-    const data = await userModel.create(newUserDetails);
 
-    return res.json(data);
+  //encrypt password
+  let hashedPassword: string;
+  try {
+    hashedPassword = await bcrypt.hash(
+      newUserDetails.password,
+      Number(SALT_ROUNDS as unknown as number)
+    );
+  } catch (err) {
+    return res.status(500).json({ error: 'unable to process request' });
+  }
+
+  //Generate jwt token
+  let jwtToken!: string;
+  try {
+    jwt.sign(
+      newUserDetails.email,
+      PRIVATE_KEY as unknown as string,
+      (err, hash) => {
+        if (err) {
+          return res.status(500).json({ error: 'unable to process request' });
+        }
+        jwtToken = hash as string;
+      }
+    );
+  } catch (err) {
+    return res.status(500).json({ error: 'unable to process request' });
+  }
+
+  //Create user and add to database
+  try {
+    const user = {
+      ...newUserDetails,
+      password: hashedPassword
+    };
+
+    const data = await userModel.create(user);
+
+    return res.status(201).json({ email: data.email, token: jwtToken });
   } catch (err) {
     const errorCode = (err as { code: number }).code;
     if (errorCode && errorCode == 11000) {
