ceccb9b7679dd06acfd6fcd412b30cb53e20b599
keside <wilsonezeala@yahoo.com>
2023-07-19 00:07:05 +0100

    WIP: create route path for marking invoice as paid

--- src/api/routes/invoices.ts
+++ src/api/routes/invoices.ts
@@ -2,13 +2,15 @@ import { Router } from 'express';
 import {
   createInvoice,
   getInvoice,
-  getInvoices
+  getInvoices,
+  payInvoice
 } from '../../controllers/invoices/invoices.controller';
 import { checkJWT } from '../../middlewares/checkJWT';
 
 const invoices = Router();
 invoices.get('/', checkJWT, getInvoices);
 invoices.get('/:invoiceId', checkJWT, getInvoice);
+invoices.patch('/:invoiceId/paid', checkJWT, payInvoice);
 
 invoices.post('/', checkJWT, createInvoice);
 
