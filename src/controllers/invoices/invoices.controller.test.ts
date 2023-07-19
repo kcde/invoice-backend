de26d4885c704fe6a87031a6d89c87a37d7b8fa0
keside <wilsonezeala@yahoo.com>
2023-07-19 09:47:21 +0100

    add test for marking invoice as paid

--- src/controllers/invoices/invoices.controller.test.ts
+++ src/controllers/invoices/invoices.controller.test.ts
@@ -5,32 +5,29 @@ import { disconnectDB, connectDB } from '../../services/database';
 const request = supertest(app);
 
 describe('Test /api/invoices', () => {
+  let authToken: string;
+  const user = {
+    email: 'johndoe@test.com',
+    password: 'lang'
+  };
+
+  const endpoint = '/api/invoices/';
+
   beforeAll(async () => {
     await connectDB();
+
+    const { body } = await request.post('/api/users').send(user);
+
+    const { token } = body;
+
+    authToken = token;
   });
 
   afterAll(async () => {
     await disconnectDB();
   });
 
-  const user = {
-    email: 'johndoe@test.com',
-    password: 'lang'
-  };
-  const user2 = {
-    email: 'johndoe2@test.com',
-    password: 'lang'
-  };
-  const user3 = {
-    email: 'johndoe3@test.com',
-    password: 'lang'
-  };
-
   it('Should return create new invoice and return a status 200', async () => {
-    const { body } = await request.post('/api/users').send(user);
-
-    const { token } = body;
-
     const response = await request
       .post('/api/invoices')
       .send({
@@ -59,30 +56,22 @@ describe('Test /api/invoices', () => {
           }
         ]
       })
-      .set('authorization', 'Bearer ' + token);
+      .set('authorization', 'Bearer ' + authToken);
 
     expect(response.status).toBe(201);
   });
 
   it('Should return all invoice of user and return status of 200', async () => {
-    const { body } = await request.post('/api/users').send(user2);
-
-    const { token } = body;
-
     const { status } = await request
       .get('/api/invoices')
-      .set('authorization', 'Bearer ' + token);
+      .set('authorization', 'Bearer ' + authToken);
 
     expect(status).toBe(200);
   });
   it('Should return an invoice and return status of 200', async () => {
-    //create user
-    const createUserResponse = await request.post('/api/users').send(user3);
-    //get toke
-    const { token } = createUserResponse.body;
     //create an invoice
     const createInvoiceResponse = await request
-      .post('/api/invoices')
+      .post(endpoint)
       .send({
         sender: {
           streetAddress: 'N0 1 west road Lugbe',
@@ -109,15 +98,59 @@ describe('Test /api/invoices', () => {
           }
         ]
       })
-      .set('authorization', 'Bearer ' + token);
+      .set('authorization', 'Bearer ' + authToken);
 
     const { status, body } = await request
       .get('/api/invoices/' + createInvoiceResponse.body.id)
-      .set('authorization', 'Bearer ' + token);
+      .set('authorization', 'Bearer ' + authToken);
 
     const invoiceId = body.id;
 
     expect(status).toBe(200);
     expect(invoiceId).toEqual(createInvoiceResponse.body.id);
+  });
+
+  it('should mark an invoice as paid', async () => {
+    //create an invoice
+    const createInvoiceResponse = await request
+      .post(endpoint)
+      .send({
+        sender: {
+          streetAddress: 'N0 1 west road Lugbe',
+          city: 'Lugbe',
+          postCode: '900010',
+          country: 'Nigeria'
+        },
+        client: {
+          name: 'keside ezeala',
+          email: 'sddcdsc@s.com',
+          streetAddress: 'N0 1 west road Lugbe',
+          city: 'Lugbe',
+          postCode: '900010',
+          country: 'Nigeria'
+        },
+        description: 'Describe',
+        issueDate: '2012-09-01',
+        paymentTerm: '7',
+        items: [
+          {
+            name: 'jdjsjd',
+            quantity: '123',
+            price: '345'
+          }
+        ]
+      })
+      .set('authorization', 'Bearer ' + authToken);
+
+    const markInvoiceAsPaid = await request
+      .patch(endpoint + createInvoiceResponse.body.id + '/paid')
+      .set('authorization', 'Bearer ' + authToken);
+
+    const updatedInvoice = await request
+      .get('/api/invoices/' + createInvoiceResponse.body.id)
+      .set('authorization', 'Bearer ' + authToken);
+
+    expect(markInvoiceAsPaid.status).toBe(200);
+    expect(updatedInvoice.body.status).toBe('paid');
   });
 });
